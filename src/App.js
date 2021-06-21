ef59c1fef609f7d97bf07715ec2d7fe6584820a0
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-21 20:21:14 +0300

    finish data request to data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import IsLoadingAndError from "./IsLoadingAndError";
 import Footer from "./Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import MyFavoriteBooks from "./MyFavoriteBooks";
+import BestBooks from "./BestBooks";
 import Login from "./Login";
 import { withAuth0 } from "@auth0/auth0-react";
 import Profile from "./components/Profile";
@@ -14,31 +14,25 @@ class App extends React.Component {
     const { user, isAuthenticated, loginWithRedirect, logout } =
       this.props.auth0;
 
-    console.log("app", this.props);
     return (
       <>
         <Router>
-          <IsLoadingAndError>
-            <Header isAuth={isAuthenticated} logoutFunc={logout} />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
-                {isAuthenticated ? (
-                  <MyFavoriteBooks />
-                ) : (
-                  <Login
-                    isAuth={isAuthenticated}
-                    loginFunc={loginWithRedirect}
-                  />
-                )}
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-              <Route path="/profile">
-                <Profile user={user} />
-              </Route>
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
+          <Header isAuth={isAuthenticated} logoutFunc={logout} />
+          <Switch>
+            <Route exact path="/">
+              {isAuthenticated ? (
+                <BestBooks userEmail={user.email} />
+              ) : (
+                <Login isAuth={isAuthenticated} loginFunc={loginWithRedirect} />
+              )}
+            </Route>
+            <Route path="/profile">
+              <Profile user={user} />
+            </Route>
+          </Switch>
+          <Footer />
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,9 +1,6 @@
 import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
 
 function LogoutButton(props) {
-  // const { isAuthenticated, logout } = useAuth0();
-
   return (
     props.isAuth && (
       <button
